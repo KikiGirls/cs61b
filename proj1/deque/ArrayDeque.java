ad4601e3384256f288189ba25820bd744928033e
Kikigirl <kikigirl@KikigirldeMacBook-Air.local>
2024-09-21 00:51:17 +0800

    done with Collatz

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,180 @@
+package deque;
+
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ArrayDeque<T> {
+    private int size;
+    private DequeIndex index;
+    private T[] array;
+    private usageRate UsageRate;  //
+
+
+    class usageRate{ //
+        private final double minUsageRate = 0.25;  // 最小使用率
+        private final double maxUsageRate = 0.80;
+
+        public void Check(){
+            double currentUsageRate = (double) size / array.length;
+            if (currentUsageRate < minUsageRate && array.length >=16){
+                sizeUpdate(array.length / 2);
+            }else if (currentUsageRate > maxUsageRate){
+                sizeUpdate(array.length * 2);
+            }
+        }
+        private void sizeUpdate(int newSize){
+            T[] newArray =(T[]) new Object[newSize];
+            if (index.IndexArray[0] + size >= array.length ){
+                System.arraycopy(array, 0, newArray, 0, index.IndexArray[1]);
+                System.arraycopy(array, index.IndexArray[0]+1, newArray, newSize -array.length -index.IndexArray[0] +1, array.length - 1 - index.IndexArray[0]);
+            }else {
+                System.arraycopy(array, index.IndexArray[0] + 1, newArray, index.IndexArray[0] + 1, size);
+            }
+            index.IndexArray = new int[]{index.IndexArray[0], index.IndexArray[1] + newSize - array.length};
+            array = newArray;
+        }
+    }
+
+    class DequeIndex{
+        public int[] IndexArray;
+        public DequeIndex(){
+            IndexArray = new int[]{array.length - 1, 0};
+        }
+
+        public void Indexupdate(int index, String operation){
+            String operation2 = index + operation;
+            switch (operation2){
+                case "1add":
+                case "0remove":
+                    IndexArray[index]++;
+                    break;
+                case "2add":
+                case "1remove":
+                    IndexArray[index]--;
+                    break;
+            }
+            if (IndexArray[index] == - 1) {
+                IndexArray[index] = array.length - 1;
+            }else if (IndexArray[index] == array.length) {
+                IndexArray[index] = 0;
+            }
+        }
+
+
+    }
+
+    public ArrayDeque() {
+        int beginsize = 8;
+        array = (T[]) new Object[beginsize];
+        size = 0;
+        index = new DequeIndex();
+    }
+
+    public ArrayDeque(T item) {
+        this();
+        this.addFirst(item);
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public void addFirst(T item) {
+        array[index.IndexArray[0]] = item;
+        size++;
+        index.Indexupdate(0, "add");
+        UsageRate.Check();
+    }
+
+    public void addLast(T item) {
+        array[index.IndexArray[1]] = item;
+        size++;
+        index.Indexupdate(1, "add");
+        UsageRate.Check();
+    }
+
+    public T removeFirst(){
+        if (isEmpty()) {
+            return null;
+        }else {
+            T item = this.array[index.IndexArray[0]];
+            index.Indexupdate(0, "remove");
+            UsageRate.Check();
+            return item;
+        }
+
+    }
+
+    public T removeLast(){
+        if (isEmpty()) {
+            return null;
+        }else {
+            T item = this.array[index.IndexArray[1]];
+            index.Indexupdate(1, "remove");
+            UsageRate.Check();
+            return item;
+        }
+
+    }
+
+    public T get(int index){
+        if (index >= this.size) {
+            return null;
+        }else if (index + this.index.IndexArray[0] < this.array.length) {
+            return this.array[index + this.index.IndexArray[0] +1];
+        }else {
+            return this.array[index + this.index.IndexArray[0] + 1 - array.length];
+        }
+    }
+
+    public void printDeque(){
+        Iterator<T> it = this.iterator();
+        for (int i = 1; i < this.size; i++) {
+            System.out.print(it.next() + " ");
+        }
+        System.out.println(it.next());
+    }
+
+    public boolean equals(Object o){
+        boolean result = false;
+        if (o instanceof ArrayDeque) {
+            ArrayDeque other = (ArrayDeque) o;
+            if (this.size == other.size) {
+                result = true;
+                for (int i = 0; i < this.size; i++) {
+                    if (this.array[i] != other.array[i]) {
+                        result = false;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public Iterator<T> iterator(){
+        return new DequeIterator();
+    }
+        private class DequeIterator implements Iterator<T>{
+            private int currentindex = index.IndexArray[0] + 1;
+            public boolean hasNext(){
+                return currentindex != index.IndexArray[1];
+            }
+            public T next(){
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }else {
+                    if (currentindex == array.length) {
+                        currentindex = 0;
+                    }
+                    currentindex++;
+                    return array[currentindex - 1];
+                }
+            }
+        }
+}
+
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,4 +1,168 @@
 package deque;
 
-public class LinkedListDeque {
+import java.util.Deque;
+import java.util.Iterator;
+
+public class LinkedListDeque<T> {
+    public class ListNode {
+        public T item;
+        public ListNode prev;
+        public ListNode next;
+
+        //构造函数
+        public ListNode(){} //哨兵结点的构造
+        public ListNode(T item){
+            this.item = item;
+        } // 值结点的构造
+    }
+
+    private ListNode sentinel;
+    private int size;
+
+    public LinkedListDeque(){
+        sentinel = new ListNode();
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        this.size = 0;
+    }
+
+    public LinkedListDeque(T item){
+        ListNode items = new ListNode(item);
+        sentinel = new ListNode();
+        sentinel.next = items;
+        sentinel.prev = items;
+        items.prev = sentinel;
+        items.next = sentinel;
+        this.size = 1;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){return size == 0;}
+
+    public void addFirst(T item){
+        ListNode addFirstNode = new ListNode(item);
+        addFirstNode.next = sentinel.next;
+        addFirstNode.prev = sentinel;
+        sentinel.next.prev = addFirstNode;
+        sentinel.next = addFirstNode;
+        this.size++;
+    }
+
+    public void addLast(T item){
+        ListNode addLastNode = new ListNode(item);
+        addLastNode.prev = sentinel.prev;
+        addLastNode.next = sentinel;
+        sentinel.prev.next = addLastNode;
+        sentinel.prev = addLastNode;
+        this.size++;
+    }
+
+    public T removeFirst(){
+        if (isEmpty()) {
+            return null;
+        }else {
+            ListNode removeFirstNode = sentinel.next;
+            sentinel.next = removeFirstNode.next;
+            removeFirstNode.next.prev = sentinel;
+            this.size--;
+            return removeFirstNode.item;
+        }
+    }
+
+    public T removeLast(){
+        if (isEmpty()) {
+            return null;
+        }else {
+            ListNode removeLastNode = sentinel.prev;
+            sentinel.prev = removeLastNode.prev;
+            removeLastNode.prev.next = sentinel;
+            this.size--;
+            return removeLastNode.item;
+        }
+    }
+
+    public T get(int index){
+        int getIndex = 0;
+        ListNode getIndex_currentNode = sentinel.next;
+        if (index < 0 || index + 1 > size) {
+            return null;
+        }else {
+            while (getIndex != index) {
+                getIndex_currentNode = getIndex_currentNode.next;
+                getIndex++;
+            }
+            return getIndex_currentNode.item;
+        }
+    }
+
+    public T getRecursive(int index) {
+        class GetRecursiveHelp {
+            public T getRecursiveHelp(ListNode t, int index) {
+                if (index < 0 || t == null) {
+                    return null;
+                } else if (index == 0) {
+                    return t.item;
+                }else {
+                    return getRecursiveHelp(t.next, index - 1);
+                }
+            }
+        }
+        GetRecursiveHelp getRecursivehelp = new GetRecursiveHelp();
+        return getRecursivehelp.getRecursiveHelp(sentinel.next, index);
+    }
+
+    public boolean equals(Object o){
+        boolean equal = false;
+        if (o instanceof LinkedListDeque) {
+            LinkedListDeque<T> other = (LinkedListDeque<T>) o;
+            if (other.size == this.size) {
+                equal = true;
+                ListNode thisNode = sentinel.next;
+                ListNode otherNode = other.sentinel.next;
+                for (int i = 0; i < this.size; i++) {
+                    if (thisNode.item.equals(otherNode.item)) {
+                        thisNode = thisNode.next;
+                        otherNode = otherNode.next;
+                    }else {
+                        equal = false;
+                    }
+                }
+            }
+
+        }
+        return equal;
+    }
+
+    public Iterator<T> iterator(){
+        return new DequeIterator();
+    }
+        private class DequeIterator implements Iterator<T>{
+            private ListNode current = sentinel.next;
+
+            public boolean hasNext(){
+                return current != null;
+            }
+
+            public T next() {
+                T ret = current.item;
+                current = current.next;
+                return ret;
+            }
+
+        }
+
+    public void printDeque(){
+        ListNode current = sentinel.next;
+        for (int i = 0; i < size ; i++) {
+            if (i == size - 1) {
+                System.out.println(current.item);
+            }else {
+                System.out.print(current.item + " ");
+                current = current.next;
+            }
+        }
+    }
 }
